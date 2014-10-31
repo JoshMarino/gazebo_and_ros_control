a8c83aa31fb378c9338c28cd60db901d33de4d65
Josh Marino <joshuamarino2015@u.northwestern.edu>
2014-10-30 19:36:49 -0500

    Added files to path to include the file name change from mini_project project to rrbot_files project, as well as CMakeList.txt and package.xml for rrrbot_files.

---
+++ rrbot_files/src/position_controller.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import rospy
+import math
+
+from std_msgs.msg import Float64
+from math import sin,cos,atan2,sqrt,fabs
+
+
+#Define a RRBot joint positions publisher for joint controllers.
+def rrbot_joint_positions_publisher():
+
+	#Initiate node for controlling joint1 and joint2 positions.
+	rospy.init_node('joint_positions_node', anonymous=True)
+
+	#Define publishers for each joint position controller commands.
+	pub1 = rospy.Publisher('/rrbot/joint1_position_controller/command', Float64, queue_size=10)
+	pub2 = rospy.Publisher('/rrbot/joint2_position_controller/command', Float64, queue_size=10)
+
+	rate = rospy.Rate(100) #100 Hz
+
+	#While loop to have joints follow a certain position, while rospy is not shutdown.
+	i = 0
+	while not rospy.is_shutdown():
+
+		#Have each joint follow a sine movement of sin(i/100).
+		sine_movement = sin(i/100.)
+
+		#Publish the same sine movement to each joint.
+		pub1.publish(sine_movement)
+		pub2.publish(sine_movement)
+
+		i = i+1 #increment i
+
+		rate.sleep() #sleep for rest of rospy.Rate(100)
+
+
+#Main section of code that will continuously run unless rospy receives interuption (ie CTRL+C)
+if __name__ == '__main__':
+	try: rrbot_joint_positions_publisher()
+	except rospy.ROSInterruptException: pass
